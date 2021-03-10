36d5f4f2de02661e3e0384670359b677dd716d5d
liuzhongh <louhtian@gmail.com>
2021-03-11 02:24:03 +0800

    Update hiwifi-c526a-part1.sh

--- ipq-40xx/hiwifi-c526a-part1.sh
+++ ipq-40xx/hiwifi-c526a-part1.sh
@@ -1,5 +1,5 @@
-# 下载新主题
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+# 下载helloworld
+git clone https://github.com/immortalwrt/helloworld.git package/helloworld
 
-# 删除旧主题
-rm -rf package/lean/luci-theme-argon
+
+
